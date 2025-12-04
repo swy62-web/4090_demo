56029e53a9d2857dfcffb83d6b967afb2ee497e5
swy62-web <swy62@umsystem.edu>
2025-12-04 17:37:45 +0000

    based

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from pathlib import Path 
+import sys
+root = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(root / "src"))
+
+from calculator_app import add
+
+def test_add_1():
+    assert add(5, 6) == 11
+
+def test_add_2():
+    assert add(5, 6) == 10
+    
